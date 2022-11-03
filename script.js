b8167edb53cdd5dbbbd8989e8afeeee3a1b4a69d
ilaha2002 <ilaha_simmi_i@inbox.ru>
2022-11-03 04:00:06 -0700

    working on design

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-document.querySelector('button').addEventListener('click', () => console.log('salam'));
+let count = 0
+
+document.querySelector('.add-button').addEventListener('click', () => { console.log(count); count++;})
+
+document.querySelector('.sort').addEventListener('click', (event) =>
+{
+    if (event.target.src.indexOf("images/downgray.svg") != -1) event.target.src = 'images/upblack.svg';
+    else event.target.src = 'images/downgray.svg';
+})
+
+document.querySelector('.delete').addEventListener('click', (event) =>
