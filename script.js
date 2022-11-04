7738c8e71e44458779a17e1fd12d263e5e4f4d0d
zegadb <zeygammehtiev@gmail.com>
2022-11-04 01:34:04 -0700

    space, test of username

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let count = 0
 
 document.querySelector('.add-button').addEventListener('click', () => { console.log(count); count++;})
-
+ 
 document.querySelector('.sort').addEventListener('click', (event) =>
 {
     if (event.target.src.indexOf("images/downgray.svg") != -1) event.target.src = 'images/upblack.svg';
